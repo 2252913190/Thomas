4704c58921f54400e6c30fc39e1133893890437b
曹悦源 <souetsugen@caoyueyuandeMBP.hitronhub.home>
2018-02-03 14:40:19 -0800

    box

---
+++ java.js
@@ -0,0 +1,13 @@
+console.log("l'm working");
+
+alert("l'm working")
+
+
+
+
+function myFunction() {
+	//var btn = document.createElement("h3");
+	//var t = document.createTextNode("touch me");
+	//btn.appendChild(t);
+	//document.body.appendChild(btn);
+}
---
+++ myscripts.js
@@ -0,0 +1,30 @@
+var x = "my message look at it";
+var u = "second message";
+var q = "third message";
+var pi=3.14;
+var m = "Mario was click"
+var theMessage = "My new message";//假设
+
+function myFunction(p1, p2) {
+    return p1 * p2;
+}
+
+function myOtherFunction(p3, p4) {
+    return p3 + p4;
+}
+
+if(pi < 3) {
+	theMessage = "This is the if statement";
+}//假设命题
+
+
+console.log(header);
+
+function changeHeader() {
+	heading = document.querySelector("h1");
+	heading.innerHTML = theMessage; //点击图片改变标题
+	
+}
+
+
+alert (m);
